b41ebd6265b27d6846a8b9c2e4eae4aaf62ec14d
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-23 19:18:28 -0500

    include loader components

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <div className="container">
+      <div className="folder">
+        <div className="top"></div>
+        <div className="bottom"></div>
+      </div>
+      <div className="title">obteniendo archivo...</div>
+    </div>
+  )
+}
+
+export default Loader;
