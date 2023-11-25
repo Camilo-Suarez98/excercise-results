19f15233cab859ca9b9c07a1ce0241ef612fbf8a
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-25 15:43:09 -0500

    include button to show more or less data and include styles and responsive design

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import ExerciseCard from "./ExerciseCard";
 
 const Results = ({ activities }) => {
+  const firstData = 30;
+  const [numData, setNumData] = useState(firstData);
+
+  const showMoreInfo = () => {
+    setNumData(numData + 30);
+  }
+
+  const showLessInfo = () => {
+    setNumData(firstData);
+  }
 
   return (
     <>
-      {
-        activities.slice(0, 40).map((activity, index) => {
-          return (
-            <ExerciseCard key={index} activity={activity} />
-          )
-        })
-      }
+      <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+        {
+          activities?.slice(0, numData)?.map((activity, index) => {
+            return (
+              <ExerciseCard key={index} activity={activity} />
+            )
+          })
+        }
+      </div>
+      <div className="mt-10 flex justify-center gap-5">
+        <button onClick={showMoreInfo} className="bg-green-500">Ver más</button>
+        <button onClick={showLessInfo} className="bg-red-500">Ver menos</button>
+      </div>
     </>
   )
 }
