e16bfa6e7f116f244e37cf015f8718e097d5eef1
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-27 10:20:56 -0500

    fix evaluations of possible fakes values

--- src/hooks/useValidateActivity.js
+++ src/hooks/useValidateActivity.js
@@ -3,34 +3,36 @@ const isSuspiciousSpeed = (activity) => {
   const maxSpeed = 3.5;
 
   const averageSpeed = activity.AverageSpeedInMetersPerSecond;
-  // console.log('averageSpeed', averageSpeed > minSpeed && averageSpeed < maxSpeed);
-  return averageSpeed > minSpeed && averageSpeed < maxSpeed;
+  return averageSpeed > minSpeed
+    && averageSpeed < maxSpeed
+    || averageSpeed === null
+    || averageSpeed === 0;
 };
 
 const isSuspiciousPace = (activity) => {
   const averagePace = activity.AveragePaceInMinutesPerKilometer;
-  // console.log('paceInMinutes', averagePace);
-  // console.log('averagePace', averagePace > 4 && averagePace < 6);
-  return averagePace > 4 && averagePace < 6;
+
+  return averagePace > 4
+    && averagePace < 6
+    || averagePace === null
+    || averagePace === 0;
 }
 
 const isSuspiciousDurationAndDistance = (activity) => {
   const calculatedSpeed = activity.DistanceInMeters / activity.DurationInSeconds;
-  // console.log('calculatedSpeed', calculatedSpeed);
-  // console.log('isSuspiciousDurationAndDistance', Math.abs(activity.AverageSpeedInMetersPerSecond - calculatedSpeed) > 0.5);
-  return Math.abs(activity.AverageSpeedInMetersPerSecond - calculatedSpeed) > 0.5;
+
+  return Math.abs(activity.AverageSpeedInMetersPerSecond - calculatedSpeed) > 0.5
+    || calculatedSpeed === null
+    || calculatedSpeed === 0;
 };
 
 const isSuspiciousStepsAndDistance = (activity) => {
   const calculatedSteps = activity.AverageSpeedInMetersPerSecond * activity.DurationInSeconds;
-  return Math.abs(activity.Steps - calculatedSteps) > 1000;
+
+  return Math.abs(activity.Steps - calculatedSteps) > 1000
+    || calculatedSteps === null
+    || calculatedSteps === 0;
 };
-
-// const isSuspiciousElevation = (activity) => {
-//   const elevationRatio = activity.TotalElevationGainInMeters / activity.DistanceInMeters;
-//   return elevationRatio < 0.001 || elevationRatio > 0.01;
-// };
-
 
 const useValidateActivity = (activity) => {
   const suspiciuousData =
@@ -38,7 +40,6 @@ const useValidateActivity = (activity) => {
     || isSuspiciousPace(activity)
     || isSuspiciousDurationAndDistance(activity)
     || isSuspiciousStepsAndDistance(activity)
-  // || isSuspiciousElevation(activity)
 
   return suspiciuousData;
 }
