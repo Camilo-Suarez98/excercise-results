82f9948f5d33a7951e38e2d174783f4aaf11c135
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-05-21 16:39:14 -0500

    fix(chore): fix error with form to enter individual data

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -13,24 +13,14 @@ const ExerciseCard = ({ activity }) => {
     AverageHeartRateInBeatsPerMinute
   } = activity;
 
-  const formatNumber = (num) => {
-    return (
-      num !== null
-        && num !== undefined
-        && typeof num === 'number'
-        ? num?.toFixed(2)
-        : '0.00'
-    )
-  };
-
   return (
     <div className={`p-3 ${!validActivity ? 'bg-green-500' : 'bg-red-500'}`}>
-      <p>Duration: {formatNumber(DurationInSeconds) / 60} min</p>
-      <p>Distance: {formatNumber(DistanceInMeters)} m</p>
-      <p>Average Pace: {formatNumber(AveragePaceInMinutesPerKilometer)} m/km</p>
+      <p>Duration: {DurationInSeconds / 60} min</p>
+      <p>Distance: {DistanceInMeters} m</p>
+      <p>Average Pace: {AveragePaceInMinutesPerKilometer} m/km</p>
       <p>Steps: {Steps}</p>
-      <p>Average Speed: {formatNumber(AverageSpeedInMetersPerSecond)} m/s</p>
-      <p>Total Elevation: {formatNumber(TotalElevationGainInMeters)} m</p>
+      <p>Average Speed: {AverageSpeedInMetersPerSecond} m/s</p>
+      <p>Total Elevation: {TotalElevationGainInMeters} m</p>
       <p>Average Heart Rate: {AverageHeartRateInBeatsPerMinute} ppm</p>
     </div>
   );
--- src/components/FormExercise.jsx
+++ src/components/FormExercise.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
 import ExerciseCard from "./ExerciseCard";
+import InputForm from "./InputForm";
 
 const FormExercise = () => {
   const [activity, setActivity] = useState({
-    DurationInSeconds: 0,
-    DistanceInMeters: 0,
-    Steps: 0,
-    AverageSpeedInMetersPerSecond: 0,
-    AveragePaceInMinutesPerKilometer: 0,
-    TotalElevationGainInMeters: 0,
-    AverageHeartRateInBeatsPerMinute: 0
+    DurationInSeconds: '',
+    DistanceInMeters: '',
+    Steps: '',
+    AverageSpeedInMetersPerSecond: '',
+    AveragePaceInMinutesPerKilometer: '',
+    TotalElevationGainInMeters: '',
+    AverageHeartRateInBeatsPerMinute: ''
   });
   const [exercises, setExercises] = useState([]);
 
@@ -28,13 +29,13 @@ const FormExercise = () => {
 
     setExercises((prevExercise) => [...prevExercise, activity]);
     setActivity({
-      DurationInSeconds: 0,
-      DistanceInMeters: 0,
-      Steps: 0,
-      AverageSpeedInMetersPerSecond: 0,
-      AveragePaceInMinutesPerKilometer: 0,
-      TotalElevationGainInMeters: 0,
-      AverageHeartRateInBeatsPerMinute: 0
+      DurationInSeconds: '',
+      DistanceInMeters: '',
+      Steps: '',
+      AverageSpeedInMetersPerSecond: '',
+      AveragePaceInMinutesPerKilometer: '',
+      TotalElevationGainInMeters: '',
+      AverageHeartRateInBeatsPerMinute: ''
     });
   };
 
@@ -42,20 +43,13 @@ const FormExercise = () => {
     <div>
       <form className="flex flex-col" onSubmit={handleSendForm}>
         <h2 className="text-3xl my-3">Enter the data</h2>
-        <label htmlFor="duration">Duration (min)</label>
-        <input id="duration" name="DurationInSeconds" type="text" onChange={handleInputChange} />
-        <label htmlFor="distance">Distance (m)</label>
-        <input id="distance" name="DistanceInMeters" type="text" onChange={handleInputChange} />
-        <label htmlFor="steps">Step</label>
-        <input id="steps" name="Steps" type="text" onChange={handleInputChange} />
-        <label htmlFor="speed">Average Speed (m/s)</label>
-        <input id="speed" name="AverageSpeedInMetersPerSecond" type="text" onChange={handleInputChange} />
-        <label htmlFor="pace">Average Pace (m/km)</label>
-        <input id="pace" name="AveragePaceInMinutesPerKilometer" type="text" onChange={handleInputChange} />
-        <label htmlFor="elevation">Total Elevation (m)</label>
-        <input id="elevation" name="TotalElevationGainInMeters" type="text" onChange={handleInputChange} />
-        <label htmlFor="heart">Heart Rate (ppm)</label>
-        <input id="heart" name="AverageHeartRateInBeatsPerMinute" type="text" onChange={handleInputChange} />
+        <InputForm id="duration" label="Duration (s)" name="DurationInSeconds" value={activity.DurationInSeconds} handleChange={handleInputChange} />
+        <InputForm id="distance" label="Distance (m)" name="DistanceInMeters" value={activity.DistanceInMeters} handleChange={handleInputChange} />
+        <InputForm id="steps" label="Step" name="Steps" value={activity.Steps} handleChange={handleInputChange} />
+        <InputForm id="speed" label="Average Speed (m/s)" name="AverageSpeedInMetersPerSecond" value={activity.AverageSpeedInMetersPerSecond} handleChange={handleInputChange} />
+        <InputForm id="pace" label="Average Pace (m/km)" name="AveragePaceInMinutesPerKilometer" value={activity.AveragePaceInMinutesPerKilometer} handleChange={handleInputChange} />
+        <InputForm id="elevation" label="Total Elevation (m)" name="TotalElevationGainInMeters" value={activity.TotalElevationGainInMeters} handleChange={handleInputChange} />
+        <InputForm id="heart" label="Heart Rate (ppm)" name="AverageHeartRateInBeatsPerMinute" value={activity.AverageHeartRateInBeatsPerMinute} handleChange={handleInputChange} />
         <button className="mt-5 transition duration-1000 hover:bg-green-500">Check</button>
       </form>
       <div className="grid gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
