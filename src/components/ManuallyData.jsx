2ada3edfe1d35bb43381eab18596b226ed59e08a
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-05-22 21:28:14 -0500

    fix: change language word

--- src/components/ManuallyData.jsx
+++ src/components/ManuallyData.jsx
@@ -1,7 +1,7 @@
 const ManuallyData = ({ onClick }) => {
   return (
     <p className="text-xl">
-      Ó
+      Or
       {" "}
       <button
         className='bg-transparent p-0 font-black border-none text-green-500 transition duration-500 hover:text-green-300'
