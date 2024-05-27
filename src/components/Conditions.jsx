075106efc49db2c0bb41850b0ec58068832342b9
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-05-27 14:54:03 -0500

    feat: include new style component for an input, change some colos and create a new component fo an anchor

--- src/components/Conditions.jsx
+++ src/components/Conditions.jsx
@@ -3,12 +3,12 @@ const Conditions = () => {
     <div className="text-xl">
       <p>
         If the box is color {" "}
-        <span className="bg-green-500 text-green-500 p-1">Green</span>
+        <span className="bg-green-500 text-transparent p-1">test</span>
         {" "} it means that the data is real
       </p>
       <p className="mt-2 mb-5">
         If the color is {" "}
-        <span className="bg-red-500 text-red-500 p-1">Red</span>
+        <span className="bg-red-500 text-transparent p-1">test</span>
         {" "} it means that some of the data does not look real
       </p>
     </div>
--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -3,7 +3,7 @@ const UploadFile = ({ onChange }) => {
     <>
       <p className="text-xl">Upload your Excel file to continue</p>
       <div className='mt-6'>
-        <input type="file" onChange={onChange} />
+        <input type="file" onChange={onChange} className="file-input file-input-bordered file-input-success w-full max-w-xs cursor-pointer" />
       </div>
     </>
   );
