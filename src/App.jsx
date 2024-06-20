5d1c956a2fa440c5f185665fff6a9a612ea7d25b
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-06-19 19:07:52 -0500

    feat: include dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,30 @@ import FormExercise from './components/FormExercise';
 import Conditions from './components/Conditions';
 import UploadFile from './components/UploadFile';
 import ManuallyData from './components/ManuallyData';
-import { ActivitiesContext } from './context/UploadFileContext';
 import DownloadFile from './components/DownloadFile';
+import { ActivitiesContext } from './context/UploadFileContext';
+import { ThemeContext } from './context/ThemeContext';
 
 function App() {
   const { activities, form, loading, handleUploadFile, handleShowForm } = useContext(ActivitiesContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   if (loading) return <Loader />;
 
   return (
-    <div>
-      <h2 className='text-5xl my-5 text-green-500 font-black p-2 lg:text-7xl'>
-        Verify <span className='text-red-500'>Fit</span> 🏃
-      </h2>
-      <p className='pb-5 text-green-300 text-lg'>App to check whether your walking results are real or fake</p>
+    <div className={`w-full h-screen flex flex-col justify-center p-0 m-0 transition duration-700 ${theme === "dark" ? "bg-[#393939] text-[#d9d8d8]" : "bg-[#d9d8d8] text-[#393939]"}`}>
+      <div className='flex flex-col justify-center items-center md:flex-row'>
+        <h2 className='text-5xl my-5 text-green-500 font-black p-2 lg:text-7xl'>
+          Verify <span className='text-red-500'>Fit</span> 🏃
+        </h2>
+        <button
+          className={`focus:outline-none ${theme === "dark" ? "bg-[#d9d8d8]" : "bg-[#393939]"}`}
+          onClick={toggleTheme}
+        >
+          {theme === "dark" ? "🌤️" : "🌙"}
+        </button>
+      </div>
+      <p className='pb-5 text-red-400 text-lg'>App to check whether your walking results are real or fake</p>
       <div>
         <Conditions />
         {activities.length === 0 ?
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ActivitiesProvider } from './context/UploadFileContext.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ActivitiesProvider>
-      <App />
-    </ActivitiesProvider>
-  </React.StrictMode>,
+    <ThemeProvider>
+      <ActivitiesProvider>
+        <App />
+      </ActivitiesProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 )
